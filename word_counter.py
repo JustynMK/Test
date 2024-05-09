7134a69249aec2fc3b2d82868699b0fe12b8123d
JustynMK <102255080+JustynMK@users.noreply.github.com>
2024-05-09 14:36:30 +0200

    Update word_counter.py

--- word_counter.py
+++ word_counter.py
@@ -5,6 +5,6 @@ def count_words_in_sentence(sentence):
     num_words = len(words)
     return num_words
 
-sentence = input("Put the sentence you most like: ")
+sentence = input("Put the sentence you most like or hate: ")
 print(f"The sentence '{sentence}' has {count_words_in_sentence(sentence)} words.")
 
