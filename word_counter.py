9338c01c4ffe954ac136f9b9a8bbc015388db52f
JustynMK <102255080+JustynMK@users.noreply.github.com>
2024-05-09 12:47:05 +0200

    Create word_counter.py

---
+++ word_counter.py
@@ -0,0 +1,9 @@
+# Word counter
+
+def count_words_in_sentence(sentence):
+    words = sentence.split()
+    num_words = len(words)
+    return num_words
+
+sentence = "This is a sample sentence."
+print(f"The sentence '{sentence}' has {count_words_in_sentence(sentence)} words.")
